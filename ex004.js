630e76d47416a43211791fa62c8c81db5be12487
Myriam Nascimento <myriannasc13@gmail.com>
2025-03-17 19:00:05 -0300

    Corrigida declaração da variável lista

--- ex004.js
+++ ex004.js
@@ -1,7 +1,7 @@
 // Solicite ao usuário cinco números, armazene-os em uma lista e, em seguida, exiba todos os números na tela.
 
 const prompt = require('prompt-sync')(); 
-lista = [];
+let lista = [];
 
 for (let i = 0; i < 5; i++){
     let numero = Number(prompt('Digite um número: '));
